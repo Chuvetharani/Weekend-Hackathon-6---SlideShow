24c0e2f2ae05cb5487bd9b0eaf13792987509684
Chuvetharani <68391363+Chuvetharani@users.noreply.github.com>
2020-11-23 18:43:29 +0530

    slideshow

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,49 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = ({ slides }) => {
+  const [index, setIndex] = useState(0);
+  const prevClick = () => {
+    if (index === 0) {
+      return;
+    }
+    setIndex(index - 1);
+  };
+
+  const nextClick = () => {
+    if (index === slides.length - 1) {
+      return;
+    }
+    setIndex(index + 1);
+  };
+
+  const restart = () => {
+    setIndex(0);
+  };
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-testid="title">{slides[index].title}</h1>
+      <p data-testid="text">{slides[index].text}</p>
+      <button
+        onClick={restart}
+        data-testid="button-restart"
+        disabled={index === 0 ? true : false}>
+        Restart
+      </button>
+      <button
+        data-testid="button-prev"
+        onClick={prevClick}
+        disabled={index === 0 ? true : false}>
+        Prev
+      </button>
+      <button
+        data-testid="button-next"
+        onClick={nextClick}
+        disabled={index === slides.length - 1 ? true : false}>
+        Next
+      </button>
+    </>
+  );
+};
 
 export default App;
